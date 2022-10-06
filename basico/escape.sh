fdd10cccb0e416da1cfa3ba846a16fca63742672
gusstavocardoso <gusstavocardoso@gmail.com>
2022-10-06 12:38:25 -0300

    add escape

---
+++ basico/escape.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Hello       World
+
+echo "Hello       World"
+
+echo "Hello   \"World\""
+
+echo "Hello   " World ""
+
+echo "This is \\ a backslash"
+
+echo "This is \" a quote and this is \\ a backslash"
